cdaa606d9dc92170e062c6b9193f619220ff708e
Marek Bařinka <xbarinka@fi.muni.cz>
2018-10-08 18:02:40 +0200

    Finished implementation

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,7 +1,10 @@
 package cz.muni.fi.pa165.currency;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -12,7 +15,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -20,7 +23,27 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        logger.trace("Calling convert method with args {}, {}, {}", sourceCurrency, targetCurrency, sourceAmount);
+        if (sourceCurrency == null) {
+            throw new IllegalArgumentException("Source currency can´t be null");
+        }
+        if (targetCurrency == null) {
+            throw new IllegalArgumentException("Target currency can´t be null");
+        }
+        if (sourceAmount == null) {
+            throw new IllegalArgumentException("Source amount can´t be null");
+        }
+        
+        try {
+            BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+            if (exchangeRate == null) {
+                logger.warn("Convert fail due missing exchange rate");
+                throw new UnknownExchangeRateException("Unknown exchange rate");
+            }
+            return exchangeRate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
+        } catch (ExternalServiceFailureException ex) {
+            logger.error("Convert fail due problem when looking for exchange rate");
+            throw new UnknownExchangeRateException("Problem when looking for exchange rate", ex);
+        }
     }
-
 }
